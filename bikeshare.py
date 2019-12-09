5f533884e6228b0d3400aa1b9c4c6b2574ce225a
Fairebdi <fairebdi@gmail.com>
2019-12-09 19:06:48 +0300

    style: delete some commints

--- bikeshare.py
+++ bikeshare.py
@@ -24,7 +24,7 @@ def get_filters():
     
     print('Hello! Let\'s explore some US bikeshare data!')
     
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+     
     city = input("select city:").strip().lower()
     while city not in [ 'chicago', 'new york city' , 'washington']:
              print('invalid inputs!')
@@ -32,9 +32,9 @@ def get_filters():
              break
 
 
-   # TO DO: get user input for month (all, january, february, ... , june)
+   
     month = input("select month:").strip().capitalize()
-    # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
+    
     day = input("select day of the week:").strip().capitalize()
     print('-'*40)
     return city, month, day
